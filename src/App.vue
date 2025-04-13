263571d22bdce740f8c10738a24e86295268b8ed
XJ <2311486045@qq.com>
2025-04-13 12:45:39 +0800

    feat: develop WaterMark component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
+import WaterMark from './components/WaterMark.vue';
 
 </script>
 
 <template>
   <HelloWorld msg="X Vue Design" />
+  <h1>Component</h1>
+  <div>
+    <p>水印</p>
+    <div>
+      <WaterMark />
+    </div>
+  </div>
 </template>
 
 <style scoped>
