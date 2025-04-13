52eb3354d78f91979036410c47b4653fac1cd235
XJ <2311486045@qq.com>
2025-04-13 17:10:59 +0800

    feat: 实现按钮、链接组件

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Developed by Vue3 and TypeScript
 
 ## Components
 ### WaterMark: 水印
+### Button: 按钮
+### Link: 链接
---
+++ src/components/Link.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+
+interface Props {
+    linkText: string
+    href?: string
+    style?: Record<string, any>
+    onClick?: () => void
+}
+
+defineProps<Props>()
+
+</script>
+
+<template>
+    <a 
+    class="link-container"
+    :href="href"
+    :style="style"
+    @click="onClick"
+    >
+        {{ linkText }}
+    </a>
+</template>
+
+<style scoped>
+.link-container {
+    font-weight: 500;
+    color: #646cff;
+    text-decoration: inherit;
+    cursor: pointer;
+}
+.link-container:hover {
+    color: #535bf2;
+}
+</style>
